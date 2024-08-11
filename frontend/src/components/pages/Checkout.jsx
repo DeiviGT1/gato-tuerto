625511a1d380035d23599493ca3eb3925d4bf62c
David <josedago1163@gmail.com>
2024-08-11 19:07:06 -0400

    frontend conexino al server corregida

--- frontend/src/components/pages/Checkout.jsx
+++ frontend/src/components/pages/Checkout.jsx
@@ -124,7 +124,7 @@ function Checkout() {
       total
     };
   
-    fetch('http://localhost:3001/checkout', {
+    fetch('https://gato-tuerto-server.vercel.app/checkout', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
