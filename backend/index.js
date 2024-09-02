bca2019cca6fd0fd93b15246e2cb753270b4b0f0
David <josedago1163@gmail.com>
2024-09-02 13:37:51 -0400

    ya se

--- backend/index.js
+++ backend/index.js
@@ -104,10 +104,10 @@ app.post('/checkout', (req, res) => {
         from: process.env.FROM_PHONE_NUMBER
       })
 
-      client_2.messages.create({
+      client.messages.create({
         body: `New Order Received:\n${orderDetails}`,
         to: process.env.TO_PHONE_NUMBER_2,
-        from: process.env.FROM_PHONE_NUMBER_2
+        from: process.env.FROM_PHONE_NUMBER
       })
 
       // client.messages.create({
