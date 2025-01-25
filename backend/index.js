a262102dd4a8e3fb3719bde385dcc4a7e3f26597
David <76451517+DeiviGT1@users.noreply.github.com>
2025-01-25 18:14:39 -0500

    agregamos numeros correctos

--- backend/index.js
+++ backend/index.js
@@ -114,18 +114,18 @@ app.post('/checkout', (req, res) => {
       // })
 
       // Santi Phone
-      client.messages.create({
-        body: `New Order Received:\n${orderDetails}`,
-        to: process.env.TO_PHONE_NUMBER_3,
-        from: process.env.FROM_PHONE_NUMBER
-      })
+      // client.messages.create({
+      //   body: `New Order Received:\n${orderDetails}`,
+      //   to: process.env.TO_PHONE_NUMBER_3,
+      //   from: process.env.FROM_PHONE_NUMBER
+      //})
 
       // Gio Phone
-      client.messages.create({
-        body: `New Order Received:\n${orderDetails}`,
-        to: process.env.TO_PHONE_NUMBER_4,
-        from: process.env.FROM_PHONE_NUMBER
-      })
+      // client.messages.create({
+      //   body: `New Order Received:\n${orderDetails}`,
+      //   to: process.env.TO_PHONE_NUMBER_4,
+      //   from: process.env.FROM_PHONE_NUMBER
+      // })
 
       // Gio son Phone
       client.messages.create({
